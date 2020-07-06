bee1983e2a2649e6e2a569417869ce23954fc7ef
ianfefe <66542714+ianfefe@users.noreply.github.com>
2020-07-06 01:15:26 -0300

    Update ndasorte.c
    Nível não buga com outros caracteres!

--- ndasorte.c
+++ ndasorte.c
@@ -1,69 +1,76 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(){
 
-	int chute;
-	srand(time(NULL));
-	int nsorte = rand() % 100;
-	int nivel;
-	int tentativas;
+  int chute;
+  srand(time(NULL));
+  int nsorte = rand() % 100;
+  char nivel;
+  int tentativas;
+  int dificuldade;
 
+  printf("\n**********************************\n");
+  printf("\n*Bem vindo ao jogo de adivinhacao*\n");
+  printf("\n**********************************\n");
+  
+  do {      
 
-	printf("**********************************\n");
-	printf("*Bem vindo ao jogo de adivinhacao*\n");
-	printf("**********************************\n");
-	printf("   Escolha a dificuldade:\n");
-	printf("1-facil  2-medio  3-dificil\n");
-	scanf("%d", &nivel);
+      dificuldade = 0;
+      printf("Escolha a dificuldade\n");
+      printf("(1)Facil  (2)Medio  (3)Dificil\n");
 
-	switch(nivel){
-		case(1):
-		tentativas = 7;
-		break;
+      scanf("%s", &nivel);
 
-		case(2):
-		tentativas = 5;
-		break;
+      switch(nivel) {
 
-		default:
-		tentativas = 3;
-		break;
+        case '1': 
+          tentativas = 7;
+          break; 
+        case '2': 
+          tentativas = 5;
+          break;
+        case '3': 
+          tentativas = 3; 
+          break;
+        default:
+          printf("Escolheu uma opção inválida\n\n");
+          dificuldade = 1;
+          break;
+           }
 
-	}
+      } while (dificuldade==1);
 
-	printf("--Agora escolha um numero de 1 a 100\n");
+  printf("--Agora escolha um numero de 1 a 100\n");
 
-	for(int i = 1; i <= tentativas; i++) {
+  for(int i = 1; i <= tentativas; i++) {
 
-	printf("+ Tentativa %d de %d + \n", i, tentativas);
-	scanf("%d", &chute);
+    printf("\n+ Tentativa %d de %d + \n", i, tentativas);
+    scanf("%d", &chute);
 
-		int maior = chute > nsorte;
-		int menor = chute < nsorte;
-		int ganhou = chute == nsorte;
+      int maior = chute > nsorte;
+      int menor = chute < nsorte;
+      int ganhou = chute == nsorte;
 
-	if(ganhou){
-		printf("Parabens, voce acertou o numero secreto (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧\n\n");
-		break;
-	}
-	else if(chute<0){
-		printf("Voce nao pode chutar numeros negativos\n\n");
-		i--;
-	}
-	else if(chute>100){
-		printf("Voce nao pode chutar numeros maior que 100\n\n");
-		i--;
-	}
-	else if(maior){
-		printf("Seu chute foi maior que o numero secreto\n\n");
-	}
-	else if(menor){
-		printf("Seu chute foi menor que o numero secreto\n\n");
-	}
-}
-	if(chute!=nsorte){
-		printf("Vocẽ perdeu ¯\\_(ツ)_/¯ Tente novamente!\n");
-	}
+    if(ganhou){
+      printf("Parabens, voce acertou o numero secreto (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧\n\n");
+      break;
+    }
+    else if(chute<0 || chute>100){
+      printf("Só vale numeros entre 0 e 100\n\n");
+      i--;
+    }
+    else if(maior){
+      printf("Seu chute foi maior que o numero secreto\n\n");
+    }
+    else if(menor){
+      printf("Seu chute foi menor que o numero secreto\n\n");
+    }
+  }
+    if(chute!=nsorte){
+      printf("Vocẽ perdeu ¯\\_(ツ)_/¯ Tente novamente!\n");
+    }
+return 0;
 }
